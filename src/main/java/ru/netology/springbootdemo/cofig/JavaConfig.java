a769a66de975c89fa651881eebd8485bcf84ed61
Rishat Suleymanov <spk_irbis@mail.ru>
2021-07-20 21:57:52 +0300

    Added annotation ConditionalOnProperty

--- src/main/java/ru/netology/springbootdemo/cofig/JavaConfig.java
+++ src/main/java/ru/netology/springbootdemo/cofig/JavaConfig.java
@@ -1,5 +1,6 @@
 package ru.netology.springbootdemo.cofig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.netology.springbootdemo.profiles.DevProfile;
@@ -9,10 +10,12 @@ import ru.netology.springbootdemo.profiles.SystemProfile;
 @Configuration
 public class JavaConfig {
     @Bean
+    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "true")
     public SystemProfile devProfile() {
         return new DevProfile();
     }
     @Bean
+    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "false")
     public SystemProfile prodProfile() {
         return new ProductionProfile();
     }
