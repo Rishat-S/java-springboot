b6d8b19a02bb0c1171c96b919137d3cc37a2879e
Rishat Suleymanov <spk_irbis@mail.ru>
2021-08-11 01:23:18 +0300

    Empty test passed

--- src/test/java/ru/netology/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/ru/netology/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,27 @@
 package ru.netology.springbootdemo;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootDemoApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Before
+    public static void setUp() {
+
+    }
 
     @Test
     void contextLoads() {
+//        ResponseEntity<String> forEntity =
+//                restTemplate.getForEntity("http://localhost:" + myapp.getMappedPort(8080), String.class);
+//        System.out.println(forEntity.getBody());
     }
 
 }
