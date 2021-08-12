fa3d8d9317effd09afdaf0ab9e3cdb329adf8ffd
Rishat Suleymanov <spk_irbis@mail.ru>
2021-08-12 19:11:58 +0300

    The first part of the test is written

--- src/test/java/ru/netology/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/ru/netology/springbootdemo/SpringBootDemoApplicationTests.java
@@ -6,21 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootDemoApplicationTests {
     @Autowired
     TestRestTemplate restTemplate;
 
+    public static GenericContainer<?> devapp = new GenericContainer<>("devapp:latest");
+//    public static GenericContainer<?> prodapp = new GenericContainer<>("prodapp");
+
     @Before
     public static void setUp() {
-
+        devapp.start();
+//        prodapp.start();
     }
 
     @Test
     void contextLoads() {
-//        ResponseEntity<String> forEntity =
-//                restTemplate.getForEntity("http://localhost:" + myapp.getMappedPort(8080), String.class);
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + devapp.getMappedPort(8080), String.class);
 //        System.out.println(forEntity.getBody());
     }
 
