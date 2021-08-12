5854b06cb10d27b9f9212df753fe242caf365873
Rishat Suleymanov <spk_irbis@mail.ru>
2021-08-12 20:52:27 +0300

    Test complite

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM openjdk:8-jdk-alpine
+FROM openjdk:11-jdk-oraclelinux8
 
-EXPOSE 8081
+EXPOSE 8080
 
 ADD target/SpringBootDemo-0.0.1-SNAPSHOT.jar myapp.jar
 
-ENTRYPOINT ["java", "-jar", "/mayapp.jar"]
+ENTRYPOINT ["java", "-jar", "/myapp.jar"]
--- src/test/java/ru/netology/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/ru/netology/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,6 +1,6 @@
 package ru.netology.springbootdemo;
 
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,24 +8,34 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.testcontainers.containers.GenericContainer;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootDemoApplicationTests {
+    public static final String LOCALHOST = "http://localhost:";
     @Autowired
     TestRestTemplate restTemplate;
 
-    public static GenericContainer<?> devapp = new GenericContainer<>("devapp:latest");
-//    public static GenericContainer<?> prodapp = new GenericContainer<>("prodapp");
+    public static GenericContainer<?> devapp = new GenericContainer<>("devapp:latest")
+            .withExposedPorts(8080);
+    public static GenericContainer<?> prodapp = new GenericContainer<>("prodapp")
+            .withExposedPorts(8081);
 
-    @Before
+    @BeforeAll
     public static void setUp() {
         devapp.start();
-//        prodapp.start();
+        prodapp.start();
     }
 
     @Test
     void contextLoads() {
-        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + devapp.getMappedPort(8080), String.class);
-//        System.out.println(forEntity.getBody());
+
+        ResponseEntity<String> forDevEntity = restTemplate.getForEntity(LOCALHOST + devapp.getMappedPort(8080) + "/profile", String.class);
+        assertEquals("Current profile is dev", forDevEntity.getBody());
+
+        ResponseEntity<String> forProdEntity = restTemplate.getForEntity(LOCALHOST + prodapp.getMappedPort(8081) + "/profile", String.class);
+        assertEquals("Current profile is production", forProdEntity.getBody());
+
     }
 
 }
